44a1a5da49467462aef51a5692d26162af38cdc0
pauloWeslleY <weslleyspfc13@outlook.com>
2022-09-14 02:22:14 -0300

    nlw_eSports Stage 2 class time 21:30

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Text style={styles.title}>What is your name?</Text>
+			<Text style={styles.title}>My name is Weslley foundation this APP</Text>
+
+			<Button title="Touch Here!" />
+			<Button title="Click Here!" />
+
+			<StatusBar style="auto" backgroundColor="#edf2f4" />
+		</View>
+	);
+}
+
+interface ButtonProps {
+	title: string;
+}
+
+function Button(props: ButtonProps) {
+	return (
+		<TouchableOpacity>
+			<Text>{props.title}</Text>
+		</TouchableOpacity>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#2b2d42',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	title: {
+		color: '#d90429',
+		fontSize: 22,
+		textTransform: 'uppercase',
+		padding: 25,
+	},
 });
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,20 +1,9 @@
-// Componentes / Propriedades
-
-interface ButtonProps {
-   title: string;
-}
-
-function Button(props: ButtonProps) {
-   return <button>{props.title}</button>;
-}
+import "./styles/main.css";
 
 function App() {
    return (
-      <div>
-         <Button title="Send 1" />
-         <Button title="Send 2" />
-         <Button title="Send 3" />
-         <Button title="Hello World" />
+      <div className="w-5">
+         <h1>NLW eSports</h1>
       </div>
    );
 }
