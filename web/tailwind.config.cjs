31e1197aded9fa951713e533adf296a16396c393
pauloWeslleY <weslleyspfc13@outlook.com>
2022-09-14 03:18:36 -0300

    nlw_eSports Stage 2 class time 40:55/2:05:11

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -2,9 +2,15 @@
 module.exports = {
    content: [
       './src/**/*.tsx',
+      './index.html'
    ],
    theme: {
-      extend: {},
+      extend: {
+         backgroundImage: {
+            galaxy: "url('./background-galaxy.png')",
+            'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 27.08%, #43E7AD 40.94%, #E1D55D 33.57%)'
+         },
+      },
    },
    plugins: [],
 }
