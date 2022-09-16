e52a566759bce98862573b064569fd02fa936d58
pauloWeslleY <weslleyspfc13@outlook.com>
2022-09-16 14:15:40 -0300

    nlw_eSports Stage 4 class-time 1:18:24

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useEffect } from 'react';
 import { View, Image, FlatList } from 'react-native';
 
 import logoImg from './../../assets/logo-nlw-esports.png';
@@ -10,6 +10,12 @@ import { GameCard } from './../../components/GameCard';
 import { GAMES } from './../../utils/games';
 
 export function Home() {
+	useEffect(() => {
+		fetch('http://192.168.10.100:3333/games')
+			.then(response => response.json())
+			.then(data => console.log(data));
+	}, []);
+
 	return (
 		<View style={styles.container}>
 			<Image source={logoImg} style={styles.logo} />
